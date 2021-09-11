f12d76f1dd3d35922186a77099d28a21182f9a4d
Ashwinimandi <ashwinimandi39@gmail.com>
2021-09-11 08:06:26 +0530

    storing multiple users

--- book.js
+++ book.js
@@ -1,6 +1,7 @@
 const form=document.getElementById('my-form');
 const btn = document.querySelector('.btn');
 const ul = document.querySelector('.items');
+const userList = document.querySelector('#users');
 //var n=document.getElementById('name').value;
 //var em=document.getElementById('email').value;
     
@@ -9,9 +10,9 @@ const ul = document.querySelector('.items');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
 
 myForm.addEventListener('submit', saveToLocalStorage);
+myForm.addEventListener('DOMContentLoaded',function(e){});
 
 function saveToLocalStorage(e)
 {
@@ -25,23 +26,25 @@ function saveToLocalStorage(e)
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
+      var linebreak=document.createElement('br');
+      
+const obj={ n,em};
+localStorage.setItem(obj.em,JSON.stringify(obj));
+const li = document.createElement('li');
+userList.appendChild(document.createTextNode(` ${obj.n} : ${obj.em}` ));
+userList.appendChild(linebreak);
 
-
-    // Clear fields
-    nameInput.value = '';
-    emailInput.value = '';
-    localStorage.setItem('name',n);
-localStorage.setItem('email',em);
+//let myobj_deserialized=JSON.parse(localStorage.getItem(myobj));
+    /*localStorage.setItem('name',n);
+localStorage.setItem('email',em);*/
   }
+  
 
   
 }
-/*localStorage.setItem('name','bob');
-console.log(localStorage.getItem('name'));
-localStorage.removeItem('name');
+/*localStorage.removeItem('name');
 sessionStorage.setItem('name','Ashwini');
 console.log(sessionStorage.getItem('name'));
 sessionStorage.removeItem('name');
 document.cookie='name=ashwini; expires='+ new Date(2020,0,6).toUTCString();
 console.log(document.cookie)*/
-
